404e4674a0a0bffde5444abc4a35dbb51857ad77
Sole Cold <cool.gegeg@gmail.com>
2023-03-19 16:07:36 +0200

    feat: comment

---
+++ main.js
@@ -0,0 +1,6 @@
+// Bits: 0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
+// Type: S  E  E  E  E  E  E  E  E  E  M   M   M   M   M   M
+// S - sign
+// E - exponent
+// M - mantissa
+// 16-bit system overall
